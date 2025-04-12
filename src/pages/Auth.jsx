dafc494521abe45943c1738e4996f19f12b4f32b
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-13 02:01:11 +0900

    feat: Auth 완료

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 import './Auth.css';
 
 function Auth() {
-  return <Outlet />;
+  return (<div id="Auth">
+    <main>
+      <Link className="logo" to="/">
+        <img className="logo" src={logo} alt="판다마켓" />
+      </Link>
+      <Outlet />
+    </main>
+  </div>);
 }
 
 export default Auth;
