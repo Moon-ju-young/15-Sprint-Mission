22f7d24fb5304e373375e2cde9196a6620269280
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-10 17:34:57 +0900

    ♻️ refactor: App.jsx 추가 (이동)

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index.jsx';
+import Items from './pages/Items.jsx';
+import Auth from './pages/Auth.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import './reset.css';
+import './color.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Index />} />
+
+        <Route path="items" element={<Items />} />
+
+        <Route element={<Auth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index.jsx';
-import Items from './pages/Items.jsx';
-import Auth from './pages/Auth.jsx';
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
-import './reset.css';
-import './color.css';
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Index />} />
-
-      <Route path="items" element={<Items />} />
-
-      <Route element={<Auth />}>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(<App/>);
