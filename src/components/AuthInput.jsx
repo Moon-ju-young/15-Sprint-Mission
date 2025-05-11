a77518ac8b2276b5512343950af0dc7ba2340019
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-12 02:10:08 +0900

    ♻️ refactor: AuthInput 내부 Input component 적용

--- src/components/AuthInput.jsx
+++ src/components/AuthInput.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
+import Input from "./Input";
 import icEyeVisible from "../assets/ic_eye_visible.svg";
 import icEyeInvisible from "../assets/ic_eye_invisible.svg";
 
-function NormalInput ({ name, type, placeholder, onFocusout, valid, wrongMessage }) {
-    return (<>
-        <input required className={valid} id={name} name={name} type={type} placeholder={placeholder} onBlur={onFocusout} />
-        <div className="wrong-message">{wrongMessage}</div>
-    </>)
-}
-
-function PasswordInput ({ name, placeholder, onFocusout, valid, wrongMessage }) {
+function PasswordInput ({ name, label, placeholder, onFocusout, valid, wrongMessage }) {
     const [isVisible, setIsVisible] = useState(false);
     const onClick = () => {
         setIsVisible((prev) => !prev);
@@ -21,44 +15,49 @@ function PasswordInput ({ name, placeholder, onFocusout, valid, wrongMessage }) 
             if (password.value === passwordCheck.value) {
                 passwordCheck.classList.add("correct");
                 passwordCheck.classList.remove("wrong");
-                passwordCheck.nextElementSibling.textContent = null;
+                passwordCheck.nextElementSibling.nextElementSibling.textContent = null;
             } else if (passwordCheck.value) {
                 passwordCheck.classList.add("wrong");
                 passwordCheck.classList.remove("correct");
-                passwordCheck.nextElementSibling.textContent = "비밀번호가 일치하지 않습니다.";
+                passwordCheck.nextElementSibling.nextElementSibling.textContent = "비밀번호가 일치하지 않습니다.";
             }
         } catch (e) {}
     }
 
     if (name === "password-check") {
-        return (<div className="password-container">
-            <input required id={name} name={name}
-                type={isVisible ? "text" : "password"} 
-                placeholder={placeholder}
-                onChange={passwordMatch}
-            />
-            <div className="wrong-message"></div>
+        return (
+        <Input required name={name} label={label}
+            className="password"
+            type={isVisible ? "text" : "password"} 
+            placeholder={placeholder}
+            onChange={passwordMatch}
+        >
             <button className="eye-btn" type="button" onClick={onClick}>
                 <img src={isVisible ? icEyeVisible : icEyeInvisible} />
             </button>
-        </div>);
+            <div className="wrong-message">{wrongMessage}</div>
+        </Input>
+        );
     }
-    return (<div className="password-container">
-        <input required className={valid} id={name} name={name}
-            type={isVisible ? "text" : "password"} 
-            placeholder={placeholder}
-            minLength="8"
-            onBlur={onFocusout}
-            onChange={passwordMatch}
-        />
-        <div className="wrong-message">{wrongMessage}</div>
+    return (
+    <Input required name={name} label={label}
+        className="password"
+        inputClassName={valid} 
+        type={isVisible ? "text" : "password"} 
+        placeholder={placeholder}
+        minLength="8"
+        onBlur={onFocusout}
+        onChange={passwordMatch}
+    >
         <button className="eye-btn" type="button" onClick={onClick}>
             <img src={isVisible ? icEyeVisible : icEyeInvisible} />
         </button>
-    </div>);
+        <div className="wrong-message">{wrongMessage}</div>
+    </Input>
+    );
 }    
 
-export default function Input ({ label, name, type="text", placeholder='', emptyWrongMessage='', invalidWrongMessage='' }) {
+export default function AuthInput ({ label, name, type="text", placeholder='', emptyWrongMessage='', invalidWrongMessage='' }) {
     const [valid, setValid] = useState('');
     const [wrongMessage, setWrongMessage] = useState(null);
     
@@ -77,12 +76,10 @@ export default function Input ({ label, name, type="text", placeholder='', empty
 
     if (type === "password") {
         return (<>
-            <label htmlFor={name}>{label}</label>
-            <PasswordInput name={name} placeholder={placeholder} onFocusout={onFocusout} valid={valid} wrongMessage={wrongMessage} />
+            <PasswordInput name={name} label={label} placeholder={placeholder} onFocusout={onFocusout} valid={valid} wrongMessage={wrongMessage} />
         </>);
     }
-    return (<>
-        <label htmlFor={name}>{label}</label>
-        <NormalInput name={name} type={type} placeholder={placeholder} onFocusout={onFocusout} valid={valid} wrongMessage={wrongMessage} />
-    </>);
+    return (<Input label={label} name={name} type={type} placeholder={placeholder} onBlur={onFocusout} inputClassName={valid}>
+        <div className="wrong-message">{wrongMessage}</div>
+    </Input>);
 }
