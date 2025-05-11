3b90f469370c4d232fdab624e5ba9527df7c4a6f
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-12 01:22:07 +0900

    ✨ feat: Input에 children prop 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,12 @@
 import styles from "./Input.module.css";
 
-function Input ({ label, name, className, inputClassName, type, ...props }) {
+function Input ({ label, name, className, inputClassName, type, children, ...props }) {
     return (<div className={styles.box+' '+className}>
         <label htmlFor={name}>{label}</label>
         {type === "textarea" 
             ? <textarea id={name} name={name} className={inputClassName} {...props} />
             : <input id={name} name={name} type={type} className={inputClassName} {...props} />}
+        {children}
     </div>);
 }
 
