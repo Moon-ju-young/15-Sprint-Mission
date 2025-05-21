5e1f2c2860548fcbebaa3e806678d74195b93437
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-21 15:30:44 +0900

    ✨ feat: data response 추가

--- src/pages/ItemProduct.jsx
+++ src/pages/ItemProduct.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProduct, getProductComments } from "../api/api";
+
 function ItemProduct() {
+    const { productId } = useParams();
+    const [data, setData] = useState();
+    const [comments, setComments] = useState([]);
+    
+    useEffect(() => {
+        (async () => setData(await getProduct({ productId })))();
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            const { list } = await getProductComments({ productId, limit: 9999 });
+            setComments(list);
+        })();
+    }, []);
+
     return (<div id="item-product"></div>);
 }
 
