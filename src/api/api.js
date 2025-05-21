1e24ebe91746d7c9df024d41c73e557c3ba6c2ba
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-21 15:30:10 +0900

    ✨ feat: getProduct, getProductComment api 함수 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,17 @@
 const BASE_URL = "https://panda-market-api.vercel.app";
 
-export async function getProducts( { page=1, pageSize=10, orderBy="recent", keyword } ) {
+export async function getProducts({ page=1, pageSize=10, orderBy="recent", keyword }) {
     const query = new URLSearchParams({ page, pageSize, orderBy }).toString();
     const products = await fetch(`${BASE_URL}/products?${query}${keyword ? "&keyword="+keyword : ''}`);
     return (await products.json());
+}
+
+export async function getProduct({ productId }) {
+    const product = await fetch(`${BASE_URL}/products/${productId}`);
+    return (await product.json());
+}
+
+export async function getProductComments({ productId, limit }) {
+    const product = await fetch(`${BASE_URL}/products/${productId}/comments?limit=${limit}`);
+    return (await product.json());
 }
