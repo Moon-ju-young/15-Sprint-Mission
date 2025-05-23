fe371e27b0234f8cbf2429d388a4b8ce5418ae0c
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-23 15:21:16 +0900

    ✨ feat: inputRef prop 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
 import styles from "./Input.module.css";
 
-function Input ({ label, name, className, inputClassName, type, children, ...props }) {
+function Input ({ label, name, className, inputClassName, type, inputRef, children, ...props }) {
     return (<div className={styles.box+' '+className}>
         <label htmlFor={name}>{label}</label>
         {type === "textarea" 
-            ? <textarea id={name} name={name} className={inputClassName} {...props} />
-            : <input id={name} name={name} type={type} className={inputClassName} {...props} />}
+            ? <textarea id={name} name={name} className={inputClassName} ref={inputRef} {...props} />
+            : <input id={name} name={name} type={type} className={inputClassName} ref={inputRef} {...props} />}
         {children}
     </div>);
 }
