1244e2ebbf312823ea6b8e769fb8273e86e7fcc7
Moon-ju-young <amymoon07@khu.ac.kr>
2025-05-24 22:16:44 +0900

    ✨ feat: api 함수 Error throw 추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,19 @@ const BASE_URL = "https://panda-market-api.vercel.app";
 
 export async function getProducts({ page=1, pageSize=10, orderBy="recent", keyword }) {
     const query = new URLSearchParams({ page, pageSize, orderBy }).toString();
-    const products = await fetch(`${BASE_URL}/products?${query}${keyword ? "&keyword="+keyword : ''}`);
-    return (await products.json());
+    const response = await fetch(`${BASE_URL}/products?${query}${keyword ? "&keyword="+keyword : ''}`);
+    if (!response.ok) { throw Error("Request Error"); }
+    return (await response.json());
 }
 
 export async function getProduct({ productId }) {
-    const product = await fetch(`${BASE_URL}/products/${productId}`);
-    return (await product.json());
+    const response = await fetch(`${BASE_URL}/products/${productId}`);
+    if (!response.ok) { throw Error("Request Error"); }
+    return (await response.json());
 }
 
 export async function getProductComments({ productId, limit }) {
-    const product = await fetch(`${BASE_URL}/products/${productId}/comments?limit=${limit}`);
-    return (await product.json());
+    const response = await fetch(`${BASE_URL}/products/${productId}/comments?limit=${limit}`);
+    if (!response.ok) { throw Error("Request Error"); }
+    return (await response.json());
 }
