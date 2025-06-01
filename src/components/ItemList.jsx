9e8689db2fcfbecbd0ec5a0253b57d6d8026898a
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-01 15:30:10 +0900

    ♻️ refactor(mentor): Item 외부 태그 Link로 변경

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import icHeart from "../assets/heart/ic_heart_small_inactive.svg";
 import styles from "./ItemList.module.css";
 
 export default function ItemList ( { rows, columns, items } ) {
-    const navigate = useNavigate();
-
     return (<section className={styles.list} style={{gridTemplate: `repeat(${rows}, 1fr) / repeat(${columns}, 1fr)`}}>
         {items.slice(0, rows*columns).map((e) => (
-            <Item key={e.id} onClick={() => navigate("/items/"+e.id)} {...e} />
+            <Item key={e.id} href={"/items/"+e.id} {...e} />
         ))}
     </section>);
 }
 
-function Item ( { name, price, favoriteCount, images, onClick } ) {
-    return (<article className={styles.item} onClick={onClick}>
+function Item ( { name, price, favoriteCount, images, href } ) {
+    return (<Link className={styles.item} to={href}>
         <div className={styles.image} style={{backgroundImage: `url(${images && images[0]})`}}></div>
         <div className={styles.text}>
             <div className={styles.title}>{name}</div>
@@ -23,5 +21,5 @@ function Item ( { name, price, favoriteCount, images, onClick } ) {
                 <span>{favoriteCount}</span>
             </div>
         </div>
-    </article>);
+    </Link>);
 }
