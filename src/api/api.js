61454291967fecca8b9ee054f7f3388340367ebf
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-02 22:47:30 +0900

    ✨ feat: getProducts 함수 keyword parameter 기본값 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://panda-market-api.vercel.app";
 
-export async function getProducts({ page=1, pageSize=10, orderBy="recent", keyword }) {
+export async function getProducts({ page=1, pageSize=10, orderBy="recent", keyword='' }) {
     const query = new URLSearchParams({ page, pageSize, orderBy }).toString();
-    const response = await fetch(`${BASE_URL}/products?${query}${keyword ? "&keyword="+keyword : ''}`);
+    const response = await fetch(`${BASE_URL}/products?${query}${keyword && "&keyword="+keyword}`);
     if (!response.ok) { throw Error("Request Error"); }
     return (await response.json());
 }
