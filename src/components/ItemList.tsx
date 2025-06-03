c85b1256add3f2e58300bd8e52c7bceb03a8585d
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-04 04:47:22 +0900

    ♻️ refactor: ItemList Component 파일 jsx > tsx 변경

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import icHeart from "../assets/heart/ic_heart_small_inactive.svg";
 import styles from "./ItemList.module.css";
+import type { Product } from "../api/apiTypes";
 
-export default function ItemList ( { rows, columns, items } ) {
+export default function ItemList ( { rows, columns, items }: { 
+    rows: number;
+    columns: number;
+    items: Product[]; 
+}) {
     return (<section className={styles.list} style={{gridTemplate: `repeat(${rows}, 1fr) / repeat(${columns}, 1fr)`}}>
         {items.slice(0, rows*columns).map((e) => (
             <Item key={e.id} href={"/items/"+e.id} {...e} />
@@ -10,7 +15,7 @@ export default function ItemList ( { rows, columns, items } ) {
     </section>);
 }
 
-function Item ( { name, price, favoriteCount, images, href } ) {
+function Item ( { name, price, favoriteCount, images, href }: { href: string } & Product ) {
     return (<Link className={styles.item} to={href}>
         <div className={styles.image} style={{backgroundImage: `url(${images && images[0]})`}}></div>
         <div className={styles.text}>
