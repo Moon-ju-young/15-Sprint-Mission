40d431bc2498ebc2bb01a458f228dba827cd4ae0
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-25 16:50:00 +0900

    ✨ feat: Btn.module.css import 및 className 적용

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -1,13 +1,14 @@
 import { ButtonHTMLAttributes } from "react";
+import styles from "./Btn.module.css";
 
 export default function Btn({
   children,
   ...props
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button {...props}>
-      <div>{children}</div>
-      <div></div>
+    <button className={styles.btn} {...props}>
+      <div className={styles.content}>{children}</div>
+      <div className={styles.shadow}></div>
     </button>
   );
 }
