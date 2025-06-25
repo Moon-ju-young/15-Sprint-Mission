aadd37f63e26cf2a8eed580e15f5ccf6d891c280
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-25 17:48:46 +0900

    ✨ feat: Btn component size 및 mode prop에 따른 내용 설정

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -9,7 +9,14 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 export default function Btn({ size = "large", mode, ...props }: Props) {
   return (
     <button className={`${styles.btn} ${styles[size]}`} {...props}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        {size === "large" &&
+          (mode === "add"
+            ? "추가하기"
+            : mode === "delete"
+            ? "삭제하기"
+            : "수정 완료")}
+      </div>
       <div className={styles.shadow}></div>
     </button>
   );
