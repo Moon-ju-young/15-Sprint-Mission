0b4fb70f7de1f35ea89502ddcad5492bd0bf23fa
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-25 16:59:13 +0900

    ♻️ refactor: Btn Props type 분리

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -1,10 +1,9 @@
 import { ButtonHTMLAttributes } from "react";
 import styles from "./Btn.module.css";
 
-export default function Btn({
-  children,
-  ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export default function Btn({ children, ...props }: Props) {
   return (
     <button className={styles.btn} {...props}>
       <div className={styles.content}>{children}</div>
