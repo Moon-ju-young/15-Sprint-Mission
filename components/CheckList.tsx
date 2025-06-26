939718c55c974beb8eb47062a78f8e51afd9f598
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-26 18:48:55 +0900

    ✨ feat: CheckList component props type 설정 및 추가

--- components/CheckList.tsx
+++ components/CheckList.tsx
@@ -1,5 +1,15 @@
+import { HTMLAttributes, MouseEventHandler, ReactNode } from "react";
 import styles from "./CheckList.module.css";
 
-export default function CheckList() {
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  isChecked: boolean;
+  onButtonClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function CheckList({
+  children,
+  isChecked,
+  onButtonClick,
+}: Props) {
   return;
 }
