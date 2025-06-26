044ce1f8da844b95ab08d33d66e0980909040010
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-26 19:18:57 +0900

    ✨ feat: CheckList component className prop 추가

--- components/CheckList.tsx
+++ components/CheckList.tsx
@@ -10,12 +10,19 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 }
 
 export default function CheckList({
+  className = "",
   children,
   isChecked,
   onButtonClick,
+  ...props
 }: Props) {
   return (
-    <div className={`${styles["check-list"]} ${styles[String(isChecked)]}`}>
+    <div
+      className={`${styles["check-list"]} ${
+        styles[String(isChecked)]
+      } ${className}`}
+      {...props}
+    >
       <button type="button" onClick={onButtonClick}>
         <Image alt="checkbox" src={isChecked ? ic_checked : ic_empty} />
       </button>
