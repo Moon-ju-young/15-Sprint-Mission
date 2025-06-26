1923ce7f23d39a9338739cb64bc420f68c4c2f60
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-27 01:29:30 +0900

    ✨ feat: Search component props 추가

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,15 @@
+import { InputHTMLAttributes } from "react";
 import styles from "./Search.module.css";
 
-export default function Search() {
+export default function Search({
+  className = "",
+  style,
+  ...props
+}: InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <label className={styles.search}>
+    <label className={`${styles.search} ${className}`} style={style}>
       <div />
-      <input placeholder="할 일을 입력해주세요" />
+      <input placeholder="할 일을 입력해주세요" {...props} />
     </label>
   );
 }
