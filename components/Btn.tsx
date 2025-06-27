1ca3b9da9458da6f56b8bb5fbf56000dd0c154f7
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-27 17:55:59 +0900

    ✨ feat: Btn component icon image 추가

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -1,4 +1,9 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, useMemo } from "react";
+import Image from "next/image";
+import ic_plus_black from "@/assets/icons/plus_black.svg";
+import ic_plus_white from "@/assets/icons/plus_white.svg";
+import ic_cross from "@/assets/icons/cross.svg";
+import ic_check from "@/assets/icons/check.svg";
 import styles from "./Btn.module.css";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -13,9 +18,21 @@ const CONTENT: Record<Props["mode"], string> = {
 };
 
 export default function Btn({ size = "large", mode, ...props }: Props) {
+  const img = useMemo(() => {
+    switch (mode) {
+      case "add":
+        return { alt: "plus", src: ic_plus_black };
+      case "delete":
+        return { alt: "cross", src: ic_cross };
+      case "edit":
+        return { alt: "check", src: ic_check };
+    }
+  }, [mode]);
+
   return (
     <button className={`${styles.btn} ${styles[size]}`} {...props}>
       <div className={`${styles.content} ${styles[mode]}`}>
+        <Image height={16} {...img} />
         {size === "large" && CONTENT[mode]}
       </div>
       <div className={styles.shadow}></div>
