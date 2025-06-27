f3666830b9ad822f42e899758f0a8f72ada1f6f5
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-27 21:57:22 +0900

    ✨ feat: index page form 기능 (할 일 추가) 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
+import { FormEvent, useRef, useState } from "react";
 import Btn from "@/components/Btn";
 import Gnb from "@/components/Gnb";
 import Search from "@/components/Search";
+import { postItem } from "@/lib/api";
 
 export default function Home() {
+  const [disabled, setDisabled] = useState(false);
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (searchRef.current?.value) {
+      try {
+        setDisabled(true);
+        await postItem({ name: searchRef.current.value });
+        searchRef.current.value = "";
+      } finally {
+        setDisabled(false);
+      }
+    }
+  };
+
   return (
     <div id="home">
       <Gnb />
       <main>
-        <form>
-          <Search />
-          <Btn mode="add" />
+        <form onSubmit={handleSubmit}>
+          <Search disabled={disabled} ref={searchRef} />
+          <Btn mode="add" disabled={disabled} />
         </form>
       </main>
     </div>
