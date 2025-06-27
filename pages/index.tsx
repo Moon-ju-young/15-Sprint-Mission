40661640b5f6f133e82783bf71661f34459992a7
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-27 22:32:55 +0900

    ✨ feat: index page getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,20 @@ import { FormEvent, useRef, useState } from "react";
 import Btn from "@/components/Btn";
 import Gnb from "@/components/Gnb";
 import Search from "@/components/Search";
-import { postItem } from "@/lib/api";
+import { getItems, postItem, ResponseItems } from "@/lib/api";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const items = await getItems();
+
+  return { props: { items } };
+}
+
+export default function Home({
+  items: initialItems,
+}: {
+  items: ResponseItems;
+}) {
+  const [items, setItems] = useState(initialItems);
   const [disabled, setDisabled] = useState(false);
   const searchRef = useRef<HTMLInputElement>(null);
 
