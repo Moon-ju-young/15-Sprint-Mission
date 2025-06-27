734ec8a0d77d2eb00a9b5b479de51a2cae41ba21
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-27 21:09:17 +0900

    ✨ feat: index page form 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
+import Btn from "@/components/Btn";
 import Gnb from "@/components/Gnb";
+import Search from "@/components/Search";
 
 export default function Home() {
   return (
     <div id="home">
       <Gnb />
-      <main></main>
+      <main>
+        <form>
+          <Search />
+          <Btn mode="add" />
+        </form>
+      </main>
     </div>
   );
 }
