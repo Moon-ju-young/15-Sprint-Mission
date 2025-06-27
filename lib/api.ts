93f35af9ff1d0e2e2031ab1e10048c5ef97330fe
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-27 16:26:19 +0900

    ✨ feat: ResponseItems type 및 getItems api 함수 추가

--- lib/api.ts
+++ lib/api.ts
@@ -3,3 +3,14 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
+
+type ResponseItems = {
+  isCompleted: boolean;
+  name: string;
+  id: number;
+}[];
+
+export async function getItems(): Promise<ResponseItems> {
+  const response = await api.get<ResponseItems>("/items");
+  return response.data;
+}
