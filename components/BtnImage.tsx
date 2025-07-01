c2e77270f0f84779c29279f59fe0a1c2fafe40b7
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-01 23:00:29 +0900

    ✨ feat: BtnImage component icon image 추가

--- components/BtnImage.tsx
+++ components/BtnImage.tsx
@@ -1,4 +1,6 @@
 import { ButtonHTMLAttributes } from "react";
+import ic_plus from "@/assets/icons/plus_big.svg";
+import ic_edit from "@/assets/icons/edit.svg";
 import styles from "./BtnImage.module.css";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
