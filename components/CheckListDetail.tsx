449b4e17a04c41f05aaf3a92464ff880cbc27104
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-02 11:48:05 +0900

    ✨ feat: CheckListDetail component 구조 형성

--- components/CheckListDetail.tsx
+++ components/CheckListDetail.tsx
@@ -1,4 +1,7 @@
 import { HTMLAttributes, MouseEventHandler } from "react";
+import Image from "next/image";
+import ic_checked from "@/assets/icons/checkbox_checked.svg";
+import ic_empty from "@/assets/icons//checkbox_empty.svg";
 import styles from "./CheckListDetail.module.css";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -14,5 +17,17 @@ export default function CheckListDetail({
   defaultValue,
   ...props
 }: Props) {
-  return;
+  return (
+    <div
+      className={`${styles["check-list"]} ${
+        styles[String(isChecked)]
+      } ${className}`}
+      {...props}
+    >
+      <button type="button" onClick={onButtonClick}>
+        <Image alt="checkbox" src={isChecked ? ic_checked : ic_empty} />
+      </button>
+      <input defaultValue={defaultValue} />
+    </div>
+  );
 }
