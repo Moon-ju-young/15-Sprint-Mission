a95c2fbb499c7027c9e608df2fb85adc2b30f602
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-07 16:37:51 +0900

    ✨ feat: item page getServerSideProps 추가

--- pages/items/[itemId].tsx
+++ pages/items/[itemId].tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { GetServerSidePropsContext } from "next";
 import Gnb from "@/components/Gnb";
+import { getItem, ResponseItem } from "@/lib/api";
 
-export default function Item() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const item = await getItem(Number(context.params?.itemId));
+
+  return { props: { item } };
+}
+
+export default function Item({ item: initialItem }: { item: ResponseItem }) {
+  const [item, setItem] = useState(initialItem);
+
   return (
     <div>
       <Gnb />
