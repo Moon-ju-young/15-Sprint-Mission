32a588e01d65c1b6b60a914fd3f478b6e513a456
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-07 18:26:19 +0900

    ✨ feat: item page file input 추가

--- pages/items/[itemId].tsx
+++ pages/items/[itemId].tsx
@@ -1,7 +1,11 @@
+import { ChangeEvent, useState } from "react";
 import { GetServerSidePropsContext } from "next";
+import Image from "next/image";
+import BtnImage from "@/components/BtnImage";
 import CheckListDetail from "@/components/CheckListDetail";
 import Gnb from "@/components/Gnb";
-import { getItem, ResponseItem } from "@/lib/api";
+import { getItem, postImage, ResponseItem } from "@/lib/api";
+import ic_img from "@/assets/images/img.png";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const item = await getItem(Number(context.params?.itemId));
@@ -10,6 +14,19 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 export default function Item({ item }: { item: ResponseItem }) {
+  const [imageUrl, setImageUrl] = useState(item.imageUrl);
+
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      try {
+        const { url } = await postImage(e.target.files[0]);
+        setImageUrl(url);
+      } catch (error) {
+        alert((error as Error).message);
+      }
+    }
+  };
+
   return (
     <div>
       <Gnb />
@@ -18,6 +35,15 @@ export default function Item({ item }: { item: ResponseItem }) {
           defaultValue={item.name}
           defaultIsChecked={item.isCompleted}
         />
+        <section>
+          <div>
+            <Image alt="image" src={imageUrl ?? ic_img} />
+            <label>
+              <BtnImage mode={imageUrl ? "edit" : "plus"} />
+              <input type="file" accept="image/*" onChange={handleChange} />
+            </label>
+          </div>
+        </section>
       </form>
     </div>
   );
